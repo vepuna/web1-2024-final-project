def5695462172a638be136ace143d1fede327c62
vepuna <alioshavepuna@gmail.com>
2024-03-16 19:48:49 +0200

    mini statistic component

---
+++ src/app/features/home-page/components/available-animals/available-animals.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-available-animals',
+  standalone: true,
+  imports: [],
+  templateUrl: './available-animals.component.html'
+})
+export class AvailableAnimalsComponent {
+
+}
--- src/app/features/home-page/home-page.component.ts
+++ src/app/features/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from "../../shared/components/header/header.component";
 import {HeaderPanelComponent} from "./components/header-panel/header-panel.component";
 import {SmallStatisticComponent} from "./components/small-statistic/small-statistic.component";
+import {AvailableAnimalsComponent} from "./components/available-animals/available-animals.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent, HeaderPanelComponent, SmallStatisticComponent],
+  imports: [HeaderComponent, HeaderPanelComponent, SmallStatisticComponent, AvailableAnimalsComponent],
   templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
